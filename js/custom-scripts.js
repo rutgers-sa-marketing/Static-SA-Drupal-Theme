aad7e89283c0402698fd71cc7c3a8f55718af686
Chandni Patel <cp773@ad-its.rutgers.edu>
2019-05-28 12:37:14 -0400

    added carousel slides to index and updated facilties page

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -12,3 +12,52 @@
     if(!$(this).closest('.panel').find('.SA-panel-collapse').hasClass('in'))
         $(this).parents('.SA-panel-heading').addClass('active');
   });
+
+//Carousel Slides 
+jQuery(document).ready(function($) {
+  //set here the speed to change the slides in the carousel or pause them
+ $('#myCarousel').carousel({
+         pause: true,
+         interval: false
+ });
+  
+//Loads the html to each slider. Write in the "div id="slide-content-x" what you want to show in each slide
+ $('#carousel-text').html($('#slide-content-0').html());
+
+ //Handles the carousel thumbnails
+$('[id^=carousel-selector-]').click( function(){
+     var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+     console.log('Change: ',id);
+     var idNum = parseInt(id);
+     $('#myCarousel').carousel(idNum);
+     
+ });
+
+ 
+ // When the carousel slides, auto update the text
+ $('#myCarousel').on('slid.bs.carousel', function (e) {
+         var id = $('.item.active').data('slide-number');
+
+          //update active thumbnail!!
+         $('.SA-slider-thumb').removeClass('active');
+         $('#carousel-selector-'+id).addClass('active');
+         
+         $('#carousel-text').html($('#slide-content-'+id).html());
+         
+         
+        
+                   
+ });
+
+});
+
+
+
+$('.SA-slider-thumb').click(function(){
+$('.SA-slider-thumb').removeClass('active');
+$(this).addClass('active');
+});
+
+
+
+
