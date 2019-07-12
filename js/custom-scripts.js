9f5938a2fc2407f37ce38dec3c9079ec3ea6c2b5
phurushekar <phurushotham@gmail.com>
2019-07-12 12:21:54 -0400

    video only displas on desktop/tab. on mobile it will be static image

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -97,6 +97,38 @@ function toggleDarkLight() {
 }
 
 
+//Script to Import HTML sections 
+function includeVid() {
+  if(window.matchMedia("(min-width: 768px)").matches){
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("add-vid");
+      if (file) {
+        /*make an HTTP request using the attribute value as the file name:*/
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            /*remove the attribute, and call this function once more:*/
+            elmnt.removeAttribute("add-vid");
+            includeHTML();
+          }
+        }      
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /*exit the function:*/
+        return;
+      }
+    }
+  }
+};
+
+
 //Add/Remove Overlay based on nav open class.
 
 //open and close nav
